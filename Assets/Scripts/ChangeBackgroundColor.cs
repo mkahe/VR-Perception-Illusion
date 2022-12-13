a324aebc27b6908c4a9faf61fa58eca3814d8881
Mehrdad Kaheh <kahe.mehrdad@gmail.com>
2022-12-13 15:58:48 +0200

    modify first scene

--- Assets/Scripts/ChangeBackgroundColor.cs
+++ Assets/Scripts/ChangeBackgroundColor.cs
@@ -4,24 +4,29 @@ using UnityEngine;
 
 public class ChangeBackgroundColor : MonoBehaviour
 {
-    public Renderer backgroundRenderer;
+    private Renderer cubeRenderer;
+    private Renderer backgroundRenderer;
+    
     public GameObject backgroundWall;
+    public GameObject cube;
     [SerializeField] private Color newColor;
-    [SerializeField] private Color[] colors;
      private int colorValue;
     // Start is called before the first frame update
     void Start()
     {
      backgroundRenderer = backgroundWall.GetComponent<Renderer>();   
+     cubeRenderer = cube.GetComponent<Renderer>();   
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        
-    }
-    public void ChangeMaterial()
+  
+    public void ChangeBackground()
     {
         backgroundRenderer.material.color = newColor;
+    }
+
+    public void ChangeCubeColor()
+    {
+        cubeRenderer.material.color = newColor;
     }
 }
